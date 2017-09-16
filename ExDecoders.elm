7c9bff0011aeef393401971c07555fed4795f39b
Michał Grygierzec <michal.grygierzec@gmail.com>
2017-09-16 18:46:45 +0200

    Decoders - draft for exercises

---
+++ ExDecoders.elm
@@ -0,0 +1,11 @@
+module ExDecoders exposing (..)
+
+import Json.Decode exposing (..)
+
+-- Reference: https://guide.elm-lang.org/interop/json.html
+-- Docs: http://package.elm-lang.org/packages/elm-lang/core/5.1.1/Json-Decode
+
+-- Part 1
+-- Primitive values
+
+
